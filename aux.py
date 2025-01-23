eddac2e5a0e08052c4fe25a2e98ba9ed4f7b8109
Sila N Mahoot <112052393+silanm@users.noreply.github.com>
2025-01-23 16:33:33 +0700

    Streamlit is up!

---
+++ aux.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import math
+import math
+
+
+class LSTMLanguageModel(nn.Module):
+    def __init__(self, vocab_size, emb_dim, hid_dim, num_layers, dropout_rate):
+        super().__init__()
+        self.num_layers = num_layers
+        self.hid_dim = hid_dim
+        self.emb_dim = emb_dim
+
+        self.embedding = nn.Embedding(vocab_size, emb_dim)
+        self.lstm = nn.LSTM(emb_dim, hid_dim, num_layers=num_layers, dropout=dropout_rate, batch_first=True)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc = nn.Linear(hid_dim, vocab_size)
+
+        self.init_weights()
+
+    def init_weights(self):
+        init_range_emb = 0.1
+        init_range_other = 1 / math.sqrt(self.hid_dim)
+        self.embedding.weight.data.uniform_(-init_range_emb, init_range_other)
+        self.fc.weight.data.uniform_(-init_range_other, init_range_other)
+        self.fc.bias.data.zero_()
+        for i in range(self.num_layers):
+            self.lstm.all_weights[i][0] = torch.FloatTensor(self.emb_dim, self.hid_dim).uniform_(-init_range_other, init_range_other)  # We
+            self.lstm.all_weights[i][1] = torch.FloatTensor(self.hid_dim, self.hid_dim).uniform_(-init_range_other, init_range_other)  # Wh
+
+    def init_hidden(self, batch_size, device):
+        hidden = torch.zeros(self.num_layers, batch_size, self.hid_dim).to(device)
+        cell = torch.zeros(self.num_layers, batch_size, self.hid_dim).to(device)
+        return hidden, cell
+
+    def detach_hidden(self, hidden):
+        hidden, cell = hidden
+        hidden = hidden.detach()
+        cell = cell.detach()
+        return hidden, cell
+
+    def forward(self, src, hidden):
+        embedding = self.dropout(self.embedding(src))
+        output, hidden = self.lstm(embedding, hidden)
+        output = self.dropout(output)
+        prediction = self.fc(output)
+        return prediction, hidden
+
+
+def generate(prompt, max_seq_len, temperature, model, tokenizer, vocab, device, seed=None):
+    if seed is not None:
+        torch.manual_seed(seed)
+    model.eval()
+    tokens = tokenizer(prompt)
+    indices = [vocab[t] for t in tokens]
+    batch_size = 1
+    hidden = model.init_hidden(batch_size, device)
+    with torch.no_grad():
+        for i in range(max_seq_len):
+            src = torch.LongTensor([indices]).to(device)
+            prediction, hidden = model(src, hidden)
+
+            probs = torch.softmax(prediction[:, -1] / temperature, dim=-1)
+            prediction = torch.multinomial(probs, num_samples=1).item()
+
+            while prediction == vocab["<unk>"]:
+                prediction = torch.multinomial(probs, num_samples=1).item()
+
+            if prediction == vocab["<eos>"]:
+                break
+
+            indices.append(prediction)
+
+    itos = vocab.get_itos()
+    tokens = [itos[i] for i in indices]
+    return tokens
---
+++ streamlit_app.py
@@ -0,0 +1,53 @@
+import os
+
+import gdown
+import streamlit as st
+import torch
+import torchtext
+
+import aux
+
+SEED = 1234
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+max_seq_len = 30
+seed = 0
+temperature = 0.5
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+
+loading = st.title("Loading Model...")
+
+if not os.path.exists("best-val-lstm_lm.pt"):
+    gdown.download(id="16QOuo_XGBRakQZ9a-Nywwy28z0o6oBlH", output="best-val-lstm_lm.pt", quiet=False)
+
+if os.path.exists("best-val-lstm_lm.pt"):
+    tokenizer = torchtext.data.utils.get_tokenizer("basic_english")
+    checkpoint = torch.load("best-val-lstm_lm.pt", weights_only=False, map_location=device)
+    model = aux.LSTMLanguageModel(
+        checkpoint["vocab_size"], checkpoint["emb_dim"], checkpoint["hid_dim"], checkpoint["num_layers"], checkpoint["dropout_rate"]
+    )
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.to(device)
+    model.eval()
+    vocab = checkpoint["vocab"]
+else:
+    st.error("Error: Model file not found.")
+    st.stop()
+
+loading.empty()
+
+
+st.title("🤔 Chatbot!?")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "Enter a prompt to start the text generation."}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    generation = aux.generate(prompt, max_seq_len, temperature, model, tokenizer, vocab, device, seed)
+    st.session_state.messages.append({"role": "assistant", "content": (" ".join(generation))})
+    st.chat_message("assistant").write((" ".join(generation)))
